ae2db2c650401d443e8558dbacfbc5d713b43c97
KNChiu <36751646+KNChiu@users.noreply.github.com>
2023-08-22 16:33:01 +0800

    使用ID建立list

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,42 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-const Title = (props) => {
+const Input = ({ input, setInput, item, setItem }) => {
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
+  const clickHandler = (e) => {
+    setItem([...item, input]);
+  };
   return (
-    // 顯示出props.content
-    <h1>{props.content}</h1>
+    <div>
+      <input type="text" onChange={inputHandler} />
+      <button onClick={clickHandler}>新增</button>
+    </div>
   );
 };
 
+const List = ({ item }) => {
+  // 取出item這個props，使用map
+  const list = item.map((i) => {
+    const id = uuidv4();
+    console.log(id);
+    return <li key={id}>{i}</li>;
+  });
+  return <ul>{list}</ul>;
+};
+
 const App = () => {
-  // 定義要傳入的字串
-  let text = 'This is title';
+  const [input, setInput] = useState('');
+  const [item, setItem] = useState([]);
+
   return (
-    <div>
-      <Title content={text} />
-    </div>
+    <>
+      <Input input={input} setInput={setInput} item={item} setItem={setItem} />
+      <List item={item} />
+    </>
   );
 };
 
